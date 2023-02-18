fc325bc792e8f1d0797a4a145ba00a14ae04498e
DarffourJoshua <darffourjoshua40@gmail.com>
2023-02-18 10:32:20 -0800

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low, high;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	for (high = 'A'; high <= 'Z'; high++)
+		putchar(high);
+			putchar('\n');
+
+	return (0);
+}
