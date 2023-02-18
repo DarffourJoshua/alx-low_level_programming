9181b71a3a33a92bc5ebd384f1f40f9edcc8f2f7
DarffourJoshua <darffourjoshua40@gmail.com>
2023-02-18 04:10:28 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,11 @@
 
 int main(void)
 {
-	int alphabet;
+	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
 		putchar (alphabet);
-		putchar('\n');
-	}
+		putchar ('\n');
+
 	return (0);
 }
