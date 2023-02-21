1c72d26348dd98ca1712c94be8cf1b1a5e33b8e8
DarffourJoshua <darffourjoshua40@gmail.com>
2023-02-21 13:53:37 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char myStr[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
+	int num;
+
+	for (num = 0; num <= 8; num++)
+	{
+		putchar (myStr[num]);
+	}
+
+	return (0);
+}
