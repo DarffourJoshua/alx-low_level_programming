4046f614b146601bce21188cba69a316d8028386
DarffourJoshua <darffourjoshua40@gmail.com>
2023-02-21 16:24:11 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,28 +3,26 @@
 /**
  * print_sign - check the code
  * @n: An input number
- * Description: A function that prints the sign of a number.
- * return: 1 if n greater than 0, 0 if n is 0, -1 if n if less than 0
+ * Description: This function prints the sign of the number
+ * return: 1 if number greater than 0, 0 if n is zero, -1 if n if less than 0
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		
 		_putchar ('+');
 		return (1);
 	}
 
 	else if (n == 0)
 	{
-		
 		_putchar ('0');
 		return (0);
 	}
 
-	else {
-		
+	else
+	{
 		_putchar ('-');
 		return (-1);
 	}
