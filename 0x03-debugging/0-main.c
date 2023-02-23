e55e7381025bde2b87c606d02bd473c1bfa7fe7a
DarffourJoshua <darffourjoshua40@gmail.com>
2023-02-23 04:32:11 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - A function to test that a given number
+ * is positve or negative
+ * return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
