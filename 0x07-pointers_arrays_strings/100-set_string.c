65b8faf084ae86bd40b8d6720a827251dac1af74
DarffourJoshua <darffourjoshua40@gmail.com>
2023-03-07 00:25:33 -0800

    value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - a function that sets the value of a pointer to a char.
+ * @s: value of a pointer
+ * @to: value of a char
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_diagsums- a function that prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: a square matrix of integers
+ * @size: an input integer with the size of matrix
+ * Return: Nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	
+}
